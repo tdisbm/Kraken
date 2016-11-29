18ae399cb84a8e6897a9ecdb9b9b38fdc8adbb76
Octavian Tufar <octaviantufar@mail.ru>
2016-11-29 23:35:33 +0200

    Added app wrapper for kraken

--- src/main/java/external/KrakenApp.java
+++ src/main/java/external/KrakenApp.java
@@ -1,24 +1,42 @@
 package external;
 
 import kraken.Kraken;
+import kraken.container.ContainerResolver;
+import kraken.unit.Extension;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public abstract class KrakenApp {
     private Kraken kraken = new Kraken();
 
     public KrakenApp() {
-        this.registerExtensions(this.kraken);
-        this.registerResources(this.kraken);
-        this.registerResolvers(this.kraken);
+        List<Extension> extensions = this.registerExtensions(new LinkedList<>());
+        List<Object> resources = this.registerResources(new LinkedList<>());
+        List<ContainerResolver> resolvers = this.registerResolvers(new LinkedList<>());
+
+        for (Extension extension: extensions) {
+            this.kraken.sink(extension);
+        }
+
+        for (Object resource: resources) {
+            this.kraken.sink(resource);
+        }
+
+        for (ContainerResolver resolver: resolvers) {
+            this.kraken.sink(resolver);
+        }
+
         this.kraken.dive();
     }
 
-    final public Kraken getKraken() {
+    public Kraken getKraken() {
         return this.kraken;
     }
 
-    protected abstract void registerExtensions(Kraken kraken);
+    protected abstract List<Extension> registerExtensions(List<Extension> extensions);
 
-    protected abstract void registerResources(Kraken kraken);
+    protected abstract List<Object> registerResources(List<Object> resources);
 
-    protected abstract void registerResolvers(Kraken kraken);
+    protected abstract List<ContainerResolver> registerResolvers(List<ContainerResolver> resolvers);
 }
